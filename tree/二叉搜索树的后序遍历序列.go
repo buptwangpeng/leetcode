f3aff766bed0eba613da6987cb219e74309260ed
wangpeng <wangpeng@duoban.com>
2022-06-09 23:08:29 +0800

    feat: add 二叉搜索树的后序遍历序列

---
+++ tree/二叉搜索树的后序遍历序列.go
@@ -0,0 +1,31 @@
+package tree
+
+// leetcode 剑指 Offer 33. 二叉搜索树的后序遍历序列
+func VerifyPostorder(postorder []int) bool {
+	if postorder == nil || len(postorder) == 0 {
+		return true
+	}
+	root := postorder[len(postorder)-1]
+	tag := false
+	res := true
+
+	leftArr := make([]int, 0)
+	rightArr := make([]int, 0)
+	for i := 0; i < len(postorder)-1; i++ {
+		if postorder[i] < root && tag {
+			res = false
+		}
+		if postorder[i] < root && !tag {
+			leftArr = append(leftArr, postorder[i])
+		}
+		if postorder[i] > root {
+			rightArr = append(rightArr, postorder[i])
+			tag = true
+		}
+
+	}
+
+	left := verifyPostorder(leftArr)
+	right := verifyPostorder(rightArr)
+	return left && right && res
+}
