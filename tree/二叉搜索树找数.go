20f59587df6b8ea2a6a3a01fc199442b9e41ca97
wangpeng <wangpeng@duoban.com>
2022-06-22 22:42:09 +0800

    feat: add 二叉搜索树插入一个数

---
+++ tree/二叉搜索树找数.go
@@ -0,0 +1,21 @@
+package tree
+
+import "leetcode/utils"
+
+// leetcode 简单 700
+
+func searchBST(root *utils.TreeNode, val int) *utils.TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Value == val {
+		return root
+	}
+	if root.Value < val {
+		return searchBST(root.RightNode, val)
+	}
+	if root.Value > val {
+		return searchBST(root.LeftNode, val)
+	}
+	return nil
+}
---
+++ tree/二叉搜索树插入一个数.go
@@ -0,0 +1,25 @@
+package tree
+
+import "leetcode/utils"
+
+// leetcode 中等 701
+func insertIntoBST(root *utils.TreeNode, val int) *utils.TreeNode {
+	// 背模版，这道题可以背会，方法感觉不能完全理解
+	// 找到空位置，插入新节点
+	if root == nil {
+		return &utils.TreeNode{Value: val}
+	}
+	if root.Value == val {
+		return root
+	}
+	// val大，应插到右子树上
+	if root.Value < val {
+		root.RightNode = insertIntoBST(root.RightNode, val)
+	}
+	// val小，应插到左子树上
+	if root.Value > val {
+		root.LeftNode = insertIntoBST(root.LeftNode, val)
+	}
+	return root
+
+}
