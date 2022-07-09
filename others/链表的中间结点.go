222fc94ae5d1bbd6992467bd137ebcb15038bdab
wangpeng <wangpeng@duoban.com>
2022-07-09 21:45:52 +0800

    feat:add 链表的中间节点

---
+++ others/链表的中间结点.go
@@ -0,0 +1,18 @@
+package others
+
+import "leetcode/utils"
+
+// leetcode 876 简单
+// 快慢双指针
+func MiddleNode(head *utils.ListNode) *utils.ListNode {
+	slow := head
+	fast := head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next
+		if fast != nil {
+			fast = fast.Next
+		}
+	}
+	return slow
+}
---
+++ utils/list.go
@@ -0,0 +1,8 @@
+package utils
+
+// mock 链表
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
